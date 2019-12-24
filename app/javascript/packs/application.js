9aebd029e4e19c5a94fd94dc1d9da2edf2006ce7
Wellington <wjas@globo.com>
2019-12-24 02:15:38 -0300

    validations to user added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+require("jquery_ujs")
 require("bootstrap-sprockets")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root    to: "static_pages#index"
   get 'sobre',    to: 'static_pages#sobre'
   get 'contato',  to: 'static_pages#contato'
+  resources :contacts
+  resources :users, only: [:new, :create]
 end
