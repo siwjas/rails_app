11db86b11b8a5b64397cea55315c1e78255f482e
Wellington <wjas@globo.com>
2019-12-25 01:37:24 -0300

    Bootstrap 4 and Jquery implemented

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
-require("jquery_ujs")
-require("bootstrap-sprockets")
+// require("jquery")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,3 +15,5 @@ require("bootstrap-sprockets")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import "bootstrap";
+import "./stylesheets/application";
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   has_secure_password
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :password, length: { minimum: 6 }
-  validates :email, presence: true, length: { maximum: 25 },
+  validates :email, presence: true, length: { maximum: 40 },
                                     format: { with: VALID_EMAIL_REGEX },
                                     uniqueness: { case_sensitive: true }
 
