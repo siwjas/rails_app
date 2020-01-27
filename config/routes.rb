fa67a7771fcafa162f9a8479eff40e515c470adc
Wellington Almeida <wjas@globo.com>
2020-01-27 03:05:07 -0300

    dinamic menu and login/logout implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root    to: "static_pages#index"
+  root to: "static_pages#index"
   get 'sobre',    to: 'static_pages#sobre'
   get 'contato',  to: 'static_pages#contato'
+  get 'entrar',   to: 'sessions#new'
+  post 'entrar',  to: 'sessions#create'
+  delete 'sair',  to: 'sessions#destroy'
+
   resources :contacts
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 end
